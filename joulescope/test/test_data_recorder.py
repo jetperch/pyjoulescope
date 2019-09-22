Fixed data recorder statistics computation when length < 1 reduction.

joulescope/test/test_data_recorder.py
@@ -17,8 +17,9 @@ Test the data recorder
 """
 
 import unittest
-from joulescope.data_recorder import DataRecorder, DataReader
+from joulescope.data_recorder import DataRecorder, DataReader, Statistics, stats_to_api
 from joulescope.stream_buffer import StreamBuffer, usb_packet_factory, usb_packet_factory_signal
+from joulescope.calibration import Calibration
 import io
 import tempfile
 import numpy as np
@@ -152,6 +153,7 @@ class TestDataRecorder(unittest.TestCase):
         packets_per_burst = 128
         bursts = int(np.ceil(samples / (SAMPLES_PER_PACKET * packets_per_burst)))
         stream_buffer = StreamBuffer(sample_rate, [100], sample_rate)
+
         fh = io.BytesIO()
         d = DataRecorder(fh, sampling_frequency=sample_rate)
         d.stream_notify(stream_buffer)
@@ -170,10 +172,94 @@ class TestDataRecorder(unittest.TestCase):
 
         return fh
 
-    def test_large_file(self):
+    def test_large_file_from_usb(self):
         sample_count = 2000000 * 2
         fh = self._create_large_file(sample_count)
         r = DataReader().open(fh)
         self.assertEqual([0, sample_count], r.sample_id_range)
         reduction = r.get_reduction()
         self.assertEqual(sample_count / 20000, len(reduction))
+
+    def create_sinusoid_data(self, sample_rate, samples):
+        x = np.arange(samples, dtype=np.float)
+        x *= (1 / sample_rate)
+        data = np.empty(samples * 2, dtype=np.uint16)
+        data[0::2] = (2000 * np.sin(2 * np.pi * 1000 * x) + 5000).astype(np.uint16)
+        data[1::2] = (2000 * np.cos(2 * np.pi * 42 * x) + 5000).astype(np.uint16)
+        np.left_shift(data, 2, out=data)
+        data_view = data[1::4]
+        np.bitwise_or(data_view, 0x20, out=data_view)
+        return data
+
+    def create_sinusoid_file(self, sample_rate, samples):
+        cal = Calibration()
+        cal.current_offset[:7] = -3000
+        cal.current_gain[:7] = [1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9]
+        cal.voltage_offset[:2] = -3000
+        cal.voltage_gain[:2] = [1e-3, 1e-4]
+        cal.data = cal.save(bytes([0] * 32))
+
+        fh = io.BytesIO()
+        d = DataRecorder(fh, sampling_frequency=sample_rate, calibration=cal)
+
+        stream_buffer = StreamBuffer(sample_rate, [100], sample_rate)
+        stream_buffer.calibration_set(cal.current_offset, cal.current_gain, cal.voltage_offset, cal.voltage_gain)
+        d.stream_notify(stream_buffer)
+        data = self.create_sinusoid_data(sample_rate, samples)
+
+        chunk_size = (sample_rate // 2) * 2
+        for i in range(0, 2 * samples, chunk_size):
+            stream_buffer.insert_raw(data[i:(i + chunk_size)])
+            stream_buffer.process()
+            d.stream_notify(stream_buffer)
+
+        d.close()
+        fh.seek(0)
+        return fh
+
+    def test_statistics_get(self):
+        #fh = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data_recording_01.jls')
+        fh = self.create_sinusoid_file(2000000, 400000)
+        r = DataReader().open(fh)
+
+        t_start, t_stop = 0.066780, 0.069004
+        k_start, k_stop = r.normalize_time_arguments(t_start, t_stop, units='seconds')
+        ranges = [
+            (0, 1000),         # trivial, direct
+            (0, 20000),        # trivial, single reduction
+            (100000, 101000),  # offset, direct
+            (100000, 120000),  # offset, ex
+            (99000, 120000),
+            (100000, 121000),
+            (99000, 121000),
+            (k_start, k_stop),
+        ]
+
+        for k_start, k_stop in ranges:
+            # print(f'range {k_start}:{k_stop}')
+            s1 = r.statistics_get(k_start, k_stop, units='samples')
+            data = r.raw(k_start, k_stop, calibrated=True)
+            i_mean = np.mean(data[:, 0])
+            np.testing.assert_allclose(s1['signals']['current']['statistics']['μ'], i_mean, rtol=0.0005)
+        r.close()
+
+    def test_cache_test(self):
+        sample_rate = 2000000
+        sample_count = sample_rate * 2
+        fh = self.create_sinusoid_file(sample_rate, sample_count)
+        r = DataReader().open(fh)
+        for step_size in [1111, 2000, 11111, 20000]:
+            # print(f'step_size = {step_size}')
+            for i in range(0, sample_count - step_size, step_size):
+                s1 = r.statistics_get(i, i + step_size, units='samples')
+                data = r.raw(i, i + step_size, calibrated=True)
+                i_mean = np.mean(data[:, 0])
+                np.testing.assert_allclose(s1['signals']['current']['statistics']['μ'], i_mean, rtol=0.0005)
+        r.close()
+
+    def test_single_sample(self):
+        fh = self.create_sinusoid_file(2000000, 400000)
+        r = DataReader().open(fh)
+        s1 = r.statistics_get(20, 20, units='samples')
+        i_mean = r.raw(20, 21, calibrated=True)[0, 0]
+        np.testing.assert_allclose(s1['signals']['current']['statistics']['μ'], i_mean, rtol=0.0005)